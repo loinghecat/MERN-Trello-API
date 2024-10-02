890f3f176fb65b7e03e4c6958726f4e33c7e46c3
LoiNghe <loinghecat@gmail.com>
2024-10-02 01:25:51 -0500

    Establish connection to DB using MongoDB

--- src/server.js
+++ src/server.js
@@ -1,28 +1,39 @@
-
+/* eslint-disable no-console */
 
 import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
+import {CONNECT_DB,GET_DB} from '~/config/mongodb'
+const START_SERVER =()=>{
+  const app = express()
 
-const app = express()
+  const hostname = 'localhost'
+  const port = 8000
 
-const hostname = 'localhost'
-const port = 8017
+  app.get('/', async (req, res) => {
+    console.log(await GET_DB().listCollections().toArray())
+    res.end('<h1>Hello World!</h1><hr>')
+  })
 
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  console.log(mapOrder(
-    [ { id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' } ],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
-  res.end('<h1>Hello World!</h1><hr>')
-})
+  app.listen(port, hostname, () => {
+    console.log(`Hello Loi Nghe, I am running at http://${ hostname }:${ port }/`)
+  })
+}
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello Trung Quan Dev, I am running at ${ hostname }:${ port }/`)
-})
+(async () => {
+  try {
+    console.log('Connecting to MongoDB...')
+    await CONNECT_DB()
+    console.log('Connected to MongoDB')
+    START_SERVER()
+  } catch (error) {
+    console.error(error)
+    process.exit(0)
+  }
+})()
+// CONNECT_DB()
+//   .then(() => console.log('Connected to MongoDB'))
+//   .then(() => START_SERVER())
+//   .catch(error => {
+//     console.error(error)
+//     process.exit(0)
+//   })
+
