7b7e229174361faf371a33d41a03d69a4151e1c0
loinghelinux <loinghecat@gmail.com>
2024-10-08 20:23:48 -0500

    Create Service Layer, Implement Slugify algorithm

---
+++ src/services/boardService.js
@@ -0,0 +1,13 @@
+import { slugify } from '~/utils/formatters'
+const createNew = async ( reqBody ) => {
+  try {
+    const newBoard = {
+      ...reqBody,
+      slug: slugify(reqBody.title)
+    }
+    return newBoard
+  } catch (error) {
+    {throw error}
+  }
+}
+export const boardService = { createNew }
