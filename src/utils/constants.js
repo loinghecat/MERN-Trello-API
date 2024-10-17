579b720a9a1ffbcad9350cc59868a122084b60ca
loinghelinux <loinghecat@gmail.com>
2024-10-16 19:24:40 -0500

    Updated related files for deploying Frontend to Vercel and Backend to Render

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,7 @@
 // list of domains that are allowed to access the API
 export const WHITELIST_DOMAINS = [
   //'http://localhost:5173/'
+  'https://mern-trello-clone.vercel.app'
 ]
 export const BOARD_TYPES = {
   PUBLIC: 'public',
